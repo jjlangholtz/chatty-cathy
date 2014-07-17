9c14ba891391e6b5443ac3985608c3ad9d6e82e3
Joshua Langholtz <jjlangholtz@gmail.com>
2014-07-17 16:36:34 -0400

    Created home page and tests

---
+++ spec/features/view_homepage_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature "User visits home page" do
+  scenario "they see the title of the page" do
+    visit root_path
+
+    expect(page).to have_title 'Chatty Cathy'
+  end
+end
